490d9a27086706e27a8e9c8a30999cccfe9a0d05
Klimenty Titov <k.titov@1440.space>
2025-03-12 11:00:04 +0300

    fix http redirect autostart with http-only startup variants

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ mod router;
 mod r#static;
 
 use cc_server_kit::cc_utils::prelude::*;
+use cc_server_kit::generic_setup::StartupVariant;
 use cc_server_kit::prelude::*;
 use cc_server_kit::salvo::server::ServerHandle;
 use cc_server_kit::startup::{get_root_router_autoinject, start_force_https_redirect, start_with_service};
@@ -57,12 +58,6 @@ async fn main() -> MResult<()> {
     });
 
     let mut reload_rx = reload_tx.subscribe();
-    let custom_shutdown = move |handle: ServerHandle, http_handle: ServerHandle| async move {
-      if reload_rx.recv().await.is_ok() {
-        handle.stop_graceful(Duration::from_secs(10));
-        http_handle.stop_graceful(Duration::from_secs(10));
-      }
-    };
 
     let file = std::fs::File::open(setup.config_file.as_deref().unwrap_or("lbrp-config.json"))
       .map_err(|_| "Can't open `lbrp-config.json`!")?;
@@ -96,35 +91,74 @@ async fn main() -> MResult<()> {
       lbrp_service = lbrp_service.catcher(salvo::catcher::Catcher::default().hoop(error_handler));
     }
 
-    let (server, handle) = start_with_service(state.clone(), &setup, lbrp_service).await.unwrap();
-    let (http_server, http_handle) = start_force_https_redirect(80, 443).await.unwrap();
+    if matches!(state.startup_variant, StartupVariant::HttpsOnly)
+      || matches!(state.startup_variant, StartupVariant::Quinn)
+      || matches!(state.startup_variant, StartupVariant::QuinnOnly)
+    {
+      let custom_shutdown = move |handle: ServerHandle, http_handle: ServerHandle| async move {
+        if reload_rx.recv().await.is_ok() {
+          handle.stop_graceful(Duration::from_secs(10));
+          http_handle.stop_graceful(Duration::from_secs(10));
+        }
+      };
 
-    let h1 = handle.clone();
-    let h2 = handle.clone();
-    let http_h1 = http_handle.clone();
-    let http_h2 = http_handle.clone();
-    let custom_handle = tokio::spawn(async move { custom_shutdown(h1, http_h1).await });
-    let default_handle = tokio::spawn(async move { default_shutdown_signal(h2, http_h2).await });
+      let (server, handle) = start_with_service(state.clone(), &setup, lbrp_service).await.unwrap();
+      let (http_server, http_handle) = start_force_https_redirect(80, 443).await.unwrap();
 
-    tracing::info!("Server is booted.");
+      let h1 = handle.clone();
+      let h2 = handle.clone();
+      let http_h1 = http_handle.clone();
+      let http_h2 = http_handle.clone();
+      let custom_handle = tokio::spawn(async move { custom_shutdown(h1, http_h1).await });
+      let default_handle = tokio::spawn(async move { default_shutdown_signal(h2, Some(http_h2)).await });
 
-    select! {
-      _ = server => tracing::info!("Server is shutdowned."),
-      _ = http_server => tracing::info!("Server is shutdowned."),
-      _ = custom_handle => tracing::info!("Server is going to reload..."),
-      _ = default_handle => std::process::exit(0),
-      res = watcher_handle => {
-        tracing::info!("Watcher handle is stopped with result `{:?}`! Exit...", res);
-        return Ok(())
-      },
+      tracing::info!("Server is booted.");
+
+      select! {
+        _ = server => tracing::info!("Server is shutdowned."),
+        _ = http_server => tracing::info!("Server is shutdowned."),
+        _ = custom_handle => tracing::info!("Server is going to reload..."),
+        _ = default_handle => std::process::exit(0),
+        res = watcher_handle => {
+          tracing::info!("Watcher handle is stopped with result `{:?}`! Exit...", res);
+          return Ok(())
+        },
+      }
+    } else {
+      let custom_shutdown = move |handle: ServerHandle| async move {
+        if reload_rx.recv().await.is_ok() {
+          handle.stop_graceful(Duration::from_secs(10));
+        }
+      };
+
+      let (server, handle) = start_with_service(state.clone(), &setup, lbrp_service).await.unwrap();
+
+      let h1 = handle.clone();
+      let h2 = handle.clone();
+      let custom_handle = tokio::spawn(async move { custom_shutdown(h1).await });
+      let default_handle = tokio::spawn(async move { default_shutdown_signal(h2, None).await });
+
+      tracing::info!("Server is booted.");
+
+      select! {
+        _ = server => tracing::info!("Server is shutdowned."),
+        _ = custom_handle => tracing::info!("Server is going to reload..."),
+        _ = default_handle => std::process::exit(0),
+        res = watcher_handle => {
+          tracing::info!("Watcher handle is stopped with result `{:?}`! Exit...", res);
+          return Ok(())
+        },
+      }
     }
   }
 }
 
-async fn default_shutdown_signal(handle: ServerHandle, http_handle: ServerHandle) {
+async fn default_shutdown_signal(handle: ServerHandle, http_handle: Option<ServerHandle>) {
   tokio::signal::ctrl_c().await.unwrap();
   tracing::info!("Shutdown with Ctrl+C requested.");
   handle.stop_graceful(None);
-  http_handle.stop_graceful(None);
+  if let Some(h) = http_handle {
+    h.stop_graceful(None);
+  }
   std::process::exit(0);
 }
